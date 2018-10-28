a1d81cd1e9163ff984e12d5653e3348a604b66a6
Chris Maynard <ctmaynar@ncsu.edu>
2018-10-28 18:57:30 -0400

    Added markers to the map

---
+++ Assets/MarkerScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MarkerScript : MonoBehaviour
+{
+
+    void Start()
+    {
+
+    }
+
+    void OnMouseDown()
+    {
+        // this object was clicked - do something
+       Debug.Log(this.gameObject);
+    }
+}
